4ad08c0859844a0da6031209e3b11bfbe1f28ba5
Luccas <luccas.leme@gmail.com>
2024-12-22 06:01:09 -0300

    build apk
    Former-commit-id: 9b76205a925f5e781e8ebd8e15e85e9fad0e3b22
    Former-commit-id: 89b4304d89a485fe4c36ed2feaf0494a89338ef4
    Former-commit-id: 6ec4cfcb61a928a2922df1bf61e7c5ca3262c8e9
    Former-commit-id: 0e153d9b1317c47ca78c48c363be874190932ad9

--- lib/utils/excel.dart
+++ lib/utils/excel.dart
@@ -1,80 +1,79 @@
-import 'package:excel/excel.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/services.dart';
-import 'package:purchase_order/controller/order_model.dart';
-import 'package:purchase_order/utils/database.dart';
+// import 'package:excel/excel.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/services.dart';
+// import 'package:purchase_order/controller/order_model.dart';
+// import 'package:purchase_order/utils/database.dart';
 
-Future<Excel> importExcelFile() async {
-  ByteData data = await rootBundle.load("assets/excel.xlsx");
-  var bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
-  var excel = Excel.decodeBytes(bytes);
+// Future<Excel> importExcelFile() async {
+//   ByteData data = await rootBundle.load("assets/excel.xlsx");
+//   var bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+//   var excel = Excel.decodeBytes(bytes);
 
-  for (var table in excel.tables.keys) {
-    print(excel.tables[table]!.maxColumns);
-    print(excel.tables[table]!.maxRows);
-    // for (var row in excel.tables[table]!.rows) {
-    //   print("$row");
-    // }
-  }
-  return excel;
-}
+//   for (var table in excel.tables.keys) {
 
-void parseExcelToOrderModel() async {
-  List<OrderModel> orders = [];
+//     // for (var row in excel.tables[table]!.rows) {
+//     //   print("$row");
+//     // }
+//   }
+//   return excel;
+// }
 
-  // Assuming the sheet name is known, e.g., 'Orders'
-  final Excel excel = await importExcelFile();
-  var sheet = excel.tables['Purchase Order - CLASS']!;
+// void parseExcelToOrderModel() async {
+//   List<OrderModel> orders = [];
 
-  // Iterate over the rows (skip the header row)
-  for (var i = 1; i < sheet.rows.length; i++) {
-    var row = sheet.rows[i];
+//   // Assuming the sheet name is known, e.g., 'Orders'
+//   final Excel excel = await importExcelFile();
+//   var sheet = excel.tables['Purchase Order - CLASS']!;
 
-    try {
-      // Parse the values from the row
-      DateTime date = DateTime.parse(
-          row[0]?.value.toString() ?? ''); // Ensure proper date format
-      String documentNumber = row[1]?.value?.toString() ?? '';
-      String status = row[2]?.value?.toString() ?? '';
-      int quantityOrdered = int.tryParse(row[3]?.value?.toString() ?? '0') ?? 0;
-      String vendorEntityId = row[4]?.value?.toString() ?? '';
-      String vendorName = row[5]?.value?.toString() ?? '';
-      int quantityBilled = int.tryParse(row[6]?.value?.toString() ?? '0') ?? 0;
-      int quantityReceived =
-          int.tryParse(row[7]?.value?.toString() ?? '0') ?? 0;
+//   // Iterate over the rows (skip the header row)
+//   for (var i = 1; i < sheet.rows.length; i++) {
+//     var row = sheet.rows[i];
 
-      // Add to the list of orders
-      orders.add(
-        OrderModel(
-          date: date,
-          documentNumber: documentNumber,
-          status: status,
-          quantityOrdered: quantityOrdered,
-          vendorEntityId: vendorEntityId,
-          vendorName: vendorName,
-          quantityBilled: quantityBilled,
-          quantityReceived: quantityReceived,
-        ),
-      );
-    } catch (e) {
-      print("Error parsing row $i: $e");
-    }
-  }
+//     try {
+//       // Parse the values from the row
+//       DateTime date = DateTime.parse(
+//           row[0]?.value.toString() ?? ''); // Ensure proper date format
+//       String documentNumber = row[1]?.value?.toString() ?? '';
+//       String status = row[2]?.value?.toString() ?? '';
+//       int quantityOrdered = int.tryParse(row[3]?.value?.toString() ?? '0') ?? 0;
+//       String vendorEntityId = row[4]?.value?.toString() ?? '';
+//       String vendorName = row[5]?.value?.toString() ?? '';
+//       int quantityBilled = int.tryParse(row[6]?.value?.toString() ?? '0') ?? 0;
+//       int quantityReceived =
+//           int.tryParse(row[7]?.value?.toString() ?? '0') ?? 0;
 
-  try {
-    final map = {
-      for (var order in orders) order.documentNumber: OrderModel.toMap(order)
-    };
+//       // Add to the list of orders
+//       orders.add(
+//         OrderModel(
+//           date: date,
+//           documentNumber: documentNumber,
+//           status: status,
+//           quantityOrdered: quantityOrdered,
+//           vendorEntityId: vendorEntityId,
+//           vendorName: vendorName,
+//           quantityBilled: quantityBilled,
+//           quantityReceived: quantityReceived,
+//         ),
+//       );
+//     } catch (e) {
+//       print("Error parsing row $i: $e");
+//     }
+//   }
 
-    db.collection('orders').doc('ordersById').set({
-      'ordersById': [map]
-    });
-  } on FirebaseException catch (e) {
-    print(e.message);
-    // Get.showSnackbar(
-    //   Alert.error(title: 'Error', message: e.message ?? 'Erro desconhecido.'),
-    // );
-  } catch (e) {
-    // Alert.error(title: 'Error', message: e.toString());
-  }
-}
+//   try {
+//     final map = {
+//       for (var order in orders) order.documentNumber: OrderModel.toMap(order)
+//     };
+
+//     db.collection('orders').doc('ordersById').set({
+//       'ordersById': [map]
+//     });
+//   } on FirebaseException catch (e) {
+//     print(e.message);
+//     // Get.showSnackbar(
+//     //   Alert.error(title: 'Error', message: e.message ?? 'Erro desconhecido.'),
+//     // );
+//   } catch (e) {
+//     // Alert.error(title: 'Error', message: e.toString());
+//   }
+// }
